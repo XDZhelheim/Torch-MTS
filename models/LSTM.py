fc229353a0f3ff78bc00756ec5f14770668773a1
XDZhelheim <601251915@qq.com>
2023-04-03 17:36:16 +0800

    update LSTM to seq2seq version

--- models/LSTM.py
+++ models/LSTM.py
@@ -1,58 +1,90 @@
 import torch.nn as nn
+import torch
 from torchinfo import summary
 
 
 class LSTM(nn.Module):
-    def __init__(self, num_nodes, in_steps, out_steps, lstm_input_dim=1, lstm_hidden_dim=64):
+    def __init__(
+        self,
+        num_nodes,
+        in_steps,
+        out_steps,
+        input_dim=1,
+        output_dim=1,
+        lstm_hidden_dim=64,
+        num_layers=3,
+        seq2seq=True,
+    ):
         super(LSTM, self).__init__()
 
         self.num_nodes = num_nodes
         self.in_steps = in_steps
         self.out_steps = out_steps
-        self.lstm_input_dim = lstm_input_dim
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         self.lstm_hidden_dim = lstm_hidden_dim
-        self.use_all_h = False
+        self.num_layers = num_layers
+        self.seq2seq = seq2seq
 
-        bi = True
-        self.lstm = nn.LSTM(
-            input_size=lstm_input_dim,
+        self.encoder = nn.LSTM(
+            input_size=input_dim,
             hidden_size=lstm_hidden_dim,
             batch_first=True,
-            num_layers=2,
-            bidirectional=bi,
+            num_layers=num_layers,
+            bidirectional=not seq2seq,  # when not using seq2seq, use bidirectional LSTM
             dropout=0,
         )
 
-        self.fc_input_dim = lstm_hidden_dim
-        if bi:
-            self.fc_input_dim *= 2
-        if self.use_all_h:
-            self.fc_input_dim *= in_steps
-        self.fc = nn.Linear(self.fc_input_dim, out_steps * lstm_input_dim)
+        if self.seq2seq:
+            self.decoder = nn.LSTM(
+                input_size=output_dim,
+                hidden_size=lstm_hidden_dim,
+                batch_first=True,
+                num_layers=num_layers,
+                bidirectional=False,
+                dropout=0,
+            )
+            self.proj = nn.Linear(lstm_hidden_dim, output_dim)
+        else:
+            self.decoder = nn.Linear(lstm_hidden_dim * 2, out_steps * output_dim)
 
     def forward(self, x):
         # x: (batch_size, in_steps, num_nodes, lstm_input_dim=1)
         x = x.transpose(1, 2).contiguous()  # (batch_size, num_nodes, in_steps, 1)
         batch_size = x.shape[0]
-        x = x.view(batch_size * self.num_nodes, self.in_steps, self.lstm_input_dim)
+        x = x.view(batch_size * self.num_nodes, self.in_steps, self.input_dim)
 
-        out, _ = self.lstm(x)
-        if self.use_all_h:
-            out = out.contiguous().view(
-                batch_size * self.num_nodes, self.fc_input_dim
-            )  # (batch_size * num_nodes, in_steps * hidden_dim) use all step's output
+        out, (h, c) = self.encoder(x)
+
+        if self.seq2seq:
+            decoder_input = torch.zeros(
+                (batch_size * self.num_nodes, 1, self.output_dim), device=x.device
+            )  # (batch_size * num_nodes, 1, output_dim)
+            out = []
+
+            for _ in range(self.out_steps):
+                decoder_input, (h, c) = self.decoder(
+                    decoder_input, (h, c)
+                )  # (batch_size * num_nodes, 1, lstm_hidden_dim)
+                decoder_input = self.proj(
+                    decoder_input
+                )  # (batch_size * num_nodes, 1, output_dim)
+                out.append(decoder_input)
+
+            out = torch.cat(
+                out, dim=1
+            )  # (batch_size * num_nodes, out_steps, output_dim)
         else:
             out = out[
                 :, -1, :
             ]  # (batch_size * num_nodes, hidden_dim) use last step's output
+            out = self.decoder(out)  # (batch_size * num_nodes, out_steps * output_dim)
 
-        out = self.fc(out).view(
-            batch_size, self.num_nodes, self.out_steps, self.lstm_input_dim
-        )
+        out = out.view(batch_size, self.num_nodes, self.out_steps, self.output_dim)
 
         return out.transpose(1, 2)
 
 
 if __name__ == "__main__":
-    model = LSTM(207, 12, 12, 1, 64)
+    model = LSTM(207, 12, 12, 1, 1, 64, seq2seq=True)
     summary(model, [128, 207, 12, 1])
