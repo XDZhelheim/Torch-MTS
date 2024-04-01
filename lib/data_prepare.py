4181b2ecbda92f50fd952dd825873bf228fbe076
XDZhelheim <601251915@qq.com>
2024-04-01 11:20:50 +0800

    update dataloader

--- lib/data_prepare.py
+++ lib/data_prepare.py
@@ -2,7 +2,6 @@ import torch
 import pandas as pd
 import numpy as np
 import os
-import sklearn.preprocessing
 from .utils import print_log, StandardScaler, vrange
 
 # ! X shape: (B, T, N, C)
@@ -16,8 +15,189 @@ def dataloader_select(name):
     elif name in ("ELECTRICITY",):
         return get_dataloaders_scaled
 
-    elif name in ("ALL_SCALED", "LTSF", "SKLEARN"):
+    elif name in ("ALL_SCALED", "LTSF"):
         return get_dataloaders_scaled
+
+
+def get_dataloaders_from_index_data(
+    data_dir,
+    in_steps=12,
+    out_steps=12,
+    tod=False,
+    dow=False,
+    y_tod=False,
+    y_dow=False,
+    batch_size=64,
+    log=None,
+):
+    data = np.load(os.path.join(data_dir, f"data.npz"))["data"].astype(np.float32)
+    index = np.load(os.path.join(data_dir, f"index_{in_steps}_{out_steps}.npz"))
+
+    x_features = [0]
+    if tod:
+        x_features.append(1)
+    if dow:
+        x_features.append(2)
+
+    y_features = [0]
+    if y_tod:
+        y_features.append(1)
+    if y_dow:
+        y_features.append(2)
+
+    train_index = index["train"]  # (num_samples, 3)
+    val_index = index["val"]
+    test_index = index["test"]
+
+    # Parallel
+    # x_train_index = vrange(train_index[:, 0], train_index[:, 1])
+    # y_train_index = vrange(train_index[:, 1], train_index[:, 2])
+    # x_val_index = vrange(val_index[:, 0], val_index[:, 1])
+    # y_val_index = vrange(val_index[:, 1], val_index[:, 2])
+    # x_test_index = vrange(test_index[:, 0], test_index[:, 1])
+    # y_test_index = vrange(test_index[:, 1], test_index[:, 2])
+
+    # x_train = data[x_train_index][..., x_features]
+    # y_train = data[y_train_index][..., y_features]
+    # x_val = data[x_val_index][..., x_features]
+    # y_val = data[y_val_index][..., y_features]
+    # x_test = data[x_test_index][..., x_features]
+    # y_test = data[y_test_index][..., y_features]
+
+    # Iterative
+    x_train = np.stack([data[idx[0] : idx[1]] for idx in train_index])[..., x_features]
+    y_train = np.stack([data[idx[1] : idx[2]] for idx in train_index])[..., y_features]
+    x_val = np.stack([data[idx[0] : idx[1]] for idx in val_index])[..., x_features]
+    y_val = np.stack([data[idx[1] : idx[2]] for idx in val_index])[..., y_features]
+    x_test = np.stack([data[idx[0] : idx[1]] for idx in test_index])[..., x_features]
+    y_test = np.stack([data[idx[1] : idx[2]] for idx in test_index])[..., y_features]
+
+    scaler = StandardScaler(mean=x_train[..., 0].mean(), std=x_train[..., 0].std())
+
+    x_train[..., 0] = scaler.transform(x_train[..., 0])
+    x_val[..., 0] = scaler.transform(x_val[..., 0])
+    x_test[..., 0] = scaler.transform(x_test[..., 0])
+
+    print_log(f"Trainset:\tx-{x_train.shape}\ty-{y_train.shape}", log=log)
+    print_log(f"Valset:  \tx-{x_val.shape}  \ty-{y_val.shape}", log=log)
+    print_log(f"Testset:\tx-{x_test.shape}\ty-{y_test.shape}", log=log)
+
+    trainset = torch.utils.data.TensorDataset(
+        torch.FloatTensor(x_train), torch.FloatTensor(y_train)
+    )
+    valset = torch.utils.data.TensorDataset(
+        torch.FloatTensor(x_val), torch.FloatTensor(y_val)
+    )
+    testset = torch.utils.data.TensorDataset(
+        torch.FloatTensor(x_test), torch.FloatTensor(y_test)
+    )
+
+    trainset_loader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True
+    )
+    valset_loader = torch.utils.data.DataLoader(
+        valset, batch_size=batch_size, shuffle=False
+    )
+    testset_loader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=False
+    )
+
+    return trainset_loader, valset_loader, testset_loader, scaler
+
+
+def get_dataloaders_scaled(
+    data_dir,
+    in_steps=12,
+    out_steps=12,
+    tod=False,
+    dow=False,
+    y_tod=False,
+    y_dow=False,
+    batch_size=64,
+    log=None,
+):
+    """
+    Warning: cannot perform inverse_transform in this version!!!
+    
+    Therefore, not compatible with STF runners.
+    """
+    data = np.load(os.path.join(data_dir, f"data.npz"))["data"].astype(np.float32)
+    index = np.load(os.path.join(data_dir, f"index_{in_steps}_{out_steps}.npz"))
+
+    x_features = [0]
+    if tod:
+        x_features.append(1)
+    if dow:
+        x_features.append(2)
+
+    y_features = [0]
+    if y_tod:
+        y_features.append(1)
+    if y_dow:
+        y_features.append(2)
+
+    train_index = index["train"]  # (num_samples, 3)
+    val_index = index["val"]
+    test_index = index["test"]
+
+    len_train = train_index[-1][1]
+    scaler = StandardScaler(
+        mean=data[:len_train, :, 0].mean(axis=0), std=data[:len_train, :, 0].std(axis=0)
+    )
+    data[..., 0] = scaler.transform(data[..., 0])
+
+    # Parallel
+    # x_train_index = vrange(train_index[:, 0], train_index[:, 1])
+    # y_train_index = vrange(train_index[:, 1], train_index[:, 2])
+    # x_val_index = vrange(val_index[:, 0], val_index[:, 1])
+    # y_val_index = vrange(val_index[:, 1], val_index[:, 2])
+    # x_test_index = vrange(test_index[:, 0], test_index[:, 1])
+    # y_test_index = vrange(test_index[:, 1], test_index[:, 2])
+
+    # x_train = data[x_train_index][..., x_features]
+    # y_train = data[y_train_index][..., y_features]
+    # x_val = data[x_val_index][..., x_features]
+    # y_val = data[y_val_index][..., y_features]
+    # x_test = data[x_test_index][..., x_features]
+    # y_test = data[y_test_index][..., y_features]
+
+    # Iterative
+    x_train = np.stack([data[idx[0] : idx[1]] for idx in train_index])[..., x_features]
+    y_train = np.stack([data[idx[1] : idx[2]] for idx in train_index])[..., y_features]
+    x_val = np.stack([data[idx[0] : idx[1]] for idx in val_index])[..., x_features]
+    y_val = np.stack([data[idx[1] : idx[2]] for idx in val_index])[..., y_features]
+    x_test = np.stack([data[idx[0] : idx[1]] for idx in test_index])[..., x_features]
+    y_test = np.stack([data[idx[1] : idx[2]] for idx in test_index])[..., y_features]
+
+    print_log(f"Trainset:\tx-{x_train.shape}\ty-{y_train.shape}", log=log)
+    print_log(f"Valset:  \tx-{x_val.shape}  \ty-{y_val.shape}", log=log)
+    print_log(f"Testset:\tx-{x_test.shape}\ty-{y_test.shape}", log=log)
+    print_log("INFO: Using scaled X and Y, only for LTSF!", log=log)
+
+    trainset = torch.utils.data.TensorDataset(
+        torch.FloatTensor(x_train), torch.FloatTensor(y_train)
+    )
+    valset = torch.utils.data.TensorDataset(
+        torch.FloatTensor(x_val), torch.FloatTensor(y_val)
+    )
+    testset = torch.utils.data.TensorDataset(
+        torch.FloatTensor(x_test), torch.FloatTensor(y_test)
+    )
+
+    trainset_loader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True
+    )
+    valset_loader = torch.utils.data.DataLoader(
+        valset, batch_size=batch_size, shuffle=False
+    )
+    testset_loader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=False
+    )
+
+    return trainset_loader, valset_loader, testset_loader, scaler
+
+
+# -------------------------------- Deprecated -------------------------------- #
 
 
 def read_df(data_path, file_type="pickle", transpose=False, log=None):
@@ -207,164 +387,6 @@ def get_dataloaders_from_tvt(
     trainset = torch.utils.data.TensorDataset(data["x_train"], data["y_train"])
     valset = torch.utils.data.TensorDataset(data["x_val"], data["y_val"])
     testset = torch.utils.data.TensorDataset(data["x_test"], data["y_test"])
-
-    trainset_loader = torch.utils.data.DataLoader(
-        trainset, batch_size=batch_size, shuffle=True
-    )
-    valset_loader = torch.utils.data.DataLoader(
-        valset, batch_size=batch_size, shuffle=False
-    )
-    testset_loader = torch.utils.data.DataLoader(
-        testset, batch_size=batch_size, shuffle=False
-    )
-
-    return trainset_loader, valset_loader, testset_loader, scaler
-
-
-def get_dataloaders_from_index_data(
-    data_dir,
-    in_steps=12,
-    out_steps=12,
-    tod=False,
-    dow=False,
-    y_tod=False,
-    y_dow=False,
-    batch_size=64,
-    log=None,
-):
-    data = np.load(os.path.join(data_dir, f"data.npz"))["data"].astype(np.float32)
-    index = np.load(os.path.join(data_dir, f"index_{in_steps}_{out_steps}.npz"))
-
-    x_features = [0]
-    if tod:
-        x_features.append(1)
-    if dow:
-        x_features.append(2)
-
-    y_features = [0]
-    if y_tod:
-        y_features.append(1)
-    if y_dow:
-        y_features.append(2)
-
-    train_index = index["train"]  # (num_samples, 3)
-    val_index = index["val"]
-    test_index = index["test"]
-
-    x_train_index = vrange(train_index[:, 0], train_index[:, 1])
-    y_train_index = vrange(train_index[:, 1], train_index[:, 2])
-    x_val_index = vrange(val_index[:, 0], val_index[:, 1])
-    y_val_index = vrange(val_index[:, 1], val_index[:, 2])
-    x_test_index = vrange(test_index[:, 0], test_index[:, 1])
-    y_test_index = vrange(test_index[:, 1], test_index[:, 2])
-
-    x_train = data[x_train_index][..., x_features]
-    y_train = data[y_train_index][..., y_features]
-    x_val = data[x_val_index][..., x_features]
-    y_val = data[y_val_index][..., y_features]
-    x_test = data[x_test_index][..., x_features]
-    y_test = data[y_test_index][..., y_features]
-
-    scaler = StandardScaler(mean=x_train[..., 0].mean(), std=x_train[..., 0].std())
-
-    x_train[..., 0] = scaler.transform(x_train[..., 0])
-    x_val[..., 0] = scaler.transform(x_val[..., 0])
-    x_test[..., 0] = scaler.transform(x_test[..., 0])
-
-    print_log(f"Trainset:\tx-{x_train.shape}\ty-{y_train.shape}", log=log)
-    print_log(f"Valset:  \tx-{x_val.shape}  \ty-{y_val.shape}", log=log)
-    print_log(f"Testset:\tx-{x_test.shape}\ty-{y_test.shape}", log=log)
-
-    trainset = torch.utils.data.TensorDataset(
-        torch.FloatTensor(x_train), torch.FloatTensor(y_train)
-    )
-    valset = torch.utils.data.TensorDataset(
-        torch.FloatTensor(x_val), torch.FloatTensor(y_val)
-    )
-    testset = torch.utils.data.TensorDataset(
-        torch.FloatTensor(x_test), torch.FloatTensor(y_test)
-    )
-
-    trainset_loader = torch.utils.data.DataLoader(
-        trainset, batch_size=batch_size, shuffle=True
-    )
-    valset_loader = torch.utils.data.DataLoader(
-        valset, batch_size=batch_size, shuffle=False
-    )
-    testset_loader = torch.utils.data.DataLoader(
-        testset, batch_size=batch_size, shuffle=False
-    )
-
-    return trainset_loader, valset_loader, testset_loader, scaler
-
-
-def get_dataloaders_scaled(
-    data_dir,
-    in_steps=12,
-    out_steps=12,
-    tod=False,
-    dow=False,
-    y_tod=False,
-    y_dow=False,
-    batch_size=64,
-    log=None,
-):
-    data = np.load(os.path.join(data_dir, f"data.npz"))["data"].astype(np.float32)
-    index = np.load(os.path.join(data_dir, f"index_{in_steps}_{out_steps}.npz"))
-
-    x_features = [0]
-    if tod:
-        x_features.append(1)
-    if dow:
-        x_features.append(2)
-
-    y_features = [0]
-    if y_tod:
-        y_features.append(1)
-    if y_dow:
-        y_features.append(2)
-
-    train_index = index["train"]  # (num_samples, 3)
-    val_index = index["val"]
-    test_index = index["test"]
-
-    train_ratio = len(train_index) / (
-        len(train_index) + len(val_index) + len(test_index)
-    )
-    train_ratio = round(train_ratio, ndigits=1)
-    len_train = round(len(data) * train_ratio)
-    scaler = sklearn.preprocessing.StandardScaler()
-    scaler.fit(data[:len_train, :, 0])
-    data[..., 0] = scaler.transform(data[..., 0])
-
-    x_train_index = vrange(train_index[:, 0], train_index[:, 1])
-    y_train_index = vrange(train_index[:, 1], train_index[:, 2])
-    x_val_index = vrange(val_index[:, 0], val_index[:, 1])
-    y_val_index = vrange(val_index[:, 1], val_index[:, 2])
-    x_test_index = vrange(test_index[:, 0], test_index[:, 1])
-    y_test_index = vrange(test_index[:, 1], test_index[:, 2])
-
-    x_train = data[x_train_index][..., x_features]
-    y_train = data[y_train_index][..., y_features]
-    x_val = data[x_val_index][..., x_features]
-    y_val = data[y_val_index][..., y_features]
-    x_test = data[x_test_index][..., x_features]
-    y_test = data[y_test_index][..., y_features]
-
-    print_log(f"Trainset:\tx-{x_train.shape}\ty-{y_train.shape}", log=log)
-    print_log(f"Valset:  \tx-{x_val.shape}  \ty-{y_val.shape}", log=log)
-    print_log(f"Testset:\tx-{x_test.shape}\ty-{y_test.shape}", log=log)
-    print_log("INFO: Using scaled X and Y for LTSF!", log=log)
-
-    trainset = torch.utils.data.TensorDataset(
-        torch.FloatTensor(x_train), torch.FloatTensor(y_train)
-    )
-    valset = torch.utils.data.TensorDataset(
-        torch.FloatTensor(x_val), torch.FloatTensor(y_val)
-    )
-    testset = torch.utils.data.TensorDataset(
-        torch.FloatTensor(x_test), torch.FloatTensor(y_test)
-    )
 
     trainset_loader = torch.utils.data.DataLoader(
         trainset, batch_size=batch_size, shuffle=True
